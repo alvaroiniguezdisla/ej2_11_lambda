d633753c9a05c4bc51376362b76700b01934ec8f
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 17:26:52 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,9 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista1= {1,2,3};
+        int[] lista2= {2,4,6};
+        System.out.println(dominio.ej2_11_lambda.productoescalar(lista1,lista2));
+    }
 }
--- src/dominio/ej2_11_lambda.java
+++ src/dominio/ej2_11_lambda.java
@@ -1,2 +1,8 @@
-package dominio;public class ej2_11_lambda {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej2_11_lambda {
+    public static int productoescalar(int[]lista1,int[]lista2){
+        return Stream.iterate(0,i->i+1).limit(lista1.length).map(i->lista1[i]*lista2[i]).reduce(0,(a,b)->a+b);
+    }
 }
